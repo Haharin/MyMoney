90b93eb23b7a02ed139b1c2eeabdee09ef311a6e
Haharin <Haharin1996@yandex.ru>
2017-12-20 17:34:41 +0300

    otchet commit

--- src/main/java/logic/StringToDateParser.java
+++ src/main/java/logic/StringToDateParser.java
@@ -1,12 +1,9 @@
 package logic;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-/**
- * Created by Haharin on 19.12.2017.
- */
+
 public class StringToDateParser {
     private static final String DD_MM_YYYY_HH_MM_SS = "dd-MM-yyyy-HH-mm-ss";
     public static LocalDateTime doIt (String stringDate){
